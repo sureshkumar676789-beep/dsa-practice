afd08630b2bf5ee08b7e0999ac0b1e996fc27bf0
sureshkumar676789-beep <sureshkumar676789@gmail.com>
2026-05-27 06:34:36 +0530

    Add Two Sum solution

---
+++ arrays/two-sum.cpp
@@ -0,0 +1,11 @@
+// Problem: Two Sum
+// Platform: LeetCode / Codeforces
+// Status: Solved
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Solved Two Sum";
+    return 0;
+}
